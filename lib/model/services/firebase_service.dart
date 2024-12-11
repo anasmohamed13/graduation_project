4d91b36b27e7601a9bc1f41660d3f974d35b7df8
anas mohamed <anas2003610@gmail.com>
2024-12-11 18:58:04 +0200

    rename Colloection

--- lib/model/services/firebase_service.dart
+++ lib/model/services/firebase_service.dart
@@ -1,11 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:garduationproject/model/user_model/user_model.dart';
 
+//comment to Gana this code to save user data in firestore
+//but the doctor have collection and parent have another collection
 class DatabaseService {
   final FirebaseFirestore firestore = FirebaseFirestore.instance;
 
   Future<void> saveUser(UserModel user) async {
-    String collection = user.userType == 'Doctor' ? 'doctors' : 'parents';
+    String collection = user.userType == 'Doctor' ? 'Doctors' : 'Parents';
     await firestore.collection(collection).doc(user.email).set(user.toJson());
   }
 }
