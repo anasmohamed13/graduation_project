5d2a59d523fce68f0b63142b7fafb754702c81ee
Gannawaell <ganawael650@gmail.com>
2025-06-15 03:16:17 +0300

    no edit

--- lib/main.dart
+++ lib/main.dart
@@ -63,7 +63,7 @@ class GatoApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: DoctorHomeScreen.routeName,
+      initialRoute: Splash.routeName,
       routes: {
         Splash.routeName: (_) => const Splash(),
         HelloPage.routeName: (_) => const HelloPage(),
